a8b72797b0cc9906bc3a95b5dd77d5c8fe75df09
Lisa <louali@student.42.fr>
2020-06-29 13:10:16 +0200

    exercice complet

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Card, Avatar } from "antd";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const { Meta } = Card;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users")
+      .then(res => res.json())
+      .then(users => this.setState({ users: this.state.users.concat(users) }));
+  }
+  fetchMoreData = () => {
+    let lastid = this.state.users[this.state.users.length - 1].id;
+    fetch("https://api.github.com/users?since=" + lastid)
+      .then(res => res.json())
+      .then(users => this.setState({ users: this.state.users.concat(users) }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> Github Users </h1>
+        <InfiniteScroll
+          dataLength={this.state.users.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.users.map((user, index) => (
+            <Card key={user.id} style={{ width: "50%" }}>
+              <Meta
+                avatar={<Avatar src={user.avatar_url} />}
+                title={user.login}
+                description={user.html_url}
+              />
+            </Card>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
